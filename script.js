5c9792e9e15bbe38c784168c66ac30ca2dd4d17b
Will Alvarez <noxwill@gmail.com>
2016-08-25 21:07:57 -0500

    v3

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 $(window).on('load', function() {
-  var bg_obj, elements, svg, texts;
+  var bg_obj, bg_white, elements, nino, svg, texts;
   svg = $('object').contents();
   elements = svg.find('.element');
   texts = svg.find('.text');
   bg_obj = svg.find('#bg_obj');
+  bg_white = svg.find('#bg_white');
+  nino = svg.find('#nino');
+  nino.hover((function() {
+    if ($(this).hasClass('active')) {
+
+    } else {
+      TweenMax.to(this, 0.2, {
+        scale: "1.1",
+        ease: Power0.easeNone,
+        transformOrigin: "50% 50%"
+      });
+    }
+  }), function() {
+    if ($(this).hasClass('active')) {
+
+    } else {
+      TweenMax.to(this, 0.1, {
+        scale: "1",
+        ease: Power0.easeNone,
+        transformOrigin: "50% 50%"
+      });
+    }
+  });
   elements.hover((function() {
     if ($(this).hasClass('active')) {
 
@@ -25,6 +48,70 @@ $(window).on('load', function() {
       });
     }
   });
+  nino.on('click', function() {
+    var elem, hideKid, i, index, j, len, numb, time;
+    hideKid = function(el, cont) {
+      elements.removeClass('toHide');
+      texts.removeClass('toHide');
+      bg_obj.removeClass('toHide');
+      bg_white.removeClass('toHide');
+      return $(el).removeClass('active');
+    };
+    if ($(this).hasClass('active')) {
+      TweenMax.to(".nino_g", 0.2, {
+        autoAlpha: 0,
+        display: "none"
+      });
+      TweenMax.to(".nino_container", 0.2, {
+        autoAlpha: 0,
+        width: "0%",
+        height: "0%",
+        display: 'none'
+      });
+      TweenMax.to(nino, 0.3, {
+        scale: "1",
+        position: "",
+        xPercent: 0,
+        yPercent: 0,
+        onComplete: hideKid,
+        onCompleteParams: [this]
+      });
+    } else {
+      $(this).addClass('active');
+      TweenMax.to(nino, 0.3, {
+        scale: "2.2",
+        position: "absolute",
+        xPercent: -150,
+        yPercent: -10
+      });
+      TweenMax.to(".nino_container", 0.3, {
+        width: "100%",
+        height: "100%",
+        autoAlpha: 1,
+        display: "block"
+      });
+      elem = $(".nino_g");
+      for (index = j = 0, len = elem.length; j < len; index = ++j) {
+        i = elem[index];
+        numb = index + 1;
+        time = (index * 10) + 0.2;
+        TweenMax.to(".nino_g" + numb, 0.3, {
+          autoAlpha: 1,
+          display: "block",
+          delay: time
+        });
+        TweenMax.to(".nino_g" + numb, 0.2, {
+          autoAlpha: 0,
+          display: "none",
+          delay: numb * 10
+        });
+      }
+      elements.addClass('toHide');
+      texts.addClass('toHide');
+      bg_obj.addClass('toHide');
+      bg_white.addClass('toHide');
+    }
+  });
   return elements.on('click', function() {
     var div, resetAll;
     div = $(this).attr('id');
@@ -34,6 +121,7 @@ $(window).on('load', function() {
       elements.removeClass('toHide');
       texts.removeClass('toHide');
       bg_obj.removeClass('toHide');
+      nino.removeClass('toHide');
       return $(el).removeClass('active');
     };
     if ($(this).hasClass('active')) {
@@ -54,7 +142,9 @@ $(window).on('load', function() {
       elements.addClass('toHide');
       texts.addClass('toHide');
       bg_obj.addClass('toHide');
+      nino.addClass('toHide');
       $(this).removeClass('toHide');
+      console.log(div);
       return TweenMax.to(div, 0.8, {
         autoAlpha: 1,
         display: 'block'
